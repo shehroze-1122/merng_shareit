5cb0b6a2dfdf6379877a52c3a732b444ca2a5faa
shehroze-1122 <stalat.bee19seecs@seecs.edu.pk>
2022-11-26 22:15:30 -0500

    Remove console log

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 const { config } = require('dotenv');
 
 config()
-console.log(process.env.CONNECTION_URL)
+
 module.exports = {
     TOKEN_SECRET: process.env.TOKEN_SECRET,
     CONNECTION_URL: process.env.CONNECTION_URL
